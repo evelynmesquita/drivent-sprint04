cb8c541d1d1792349e5b10bd6ea6a988499f03ea
Evelyn <evelynbandeiramesquita@gmail.com>
2023-10-09 15:43:54 -0300

    fix: tests booking

--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -30,14 +30,14 @@ describe('GET /booking', () => {
   });
 
   it('should respond with status 401 if given token is not valid', async () => {
-    const token = faker.lorem.word();
+    const token = jwt.sign({ userId: 1 }, process.env.JWT_SECRET);
     const response = await server.get('/booking').set('Authorization', `Bearer ${token}`);
     expect(response.status).toBe(httpStatus.UNAUTHORIZED);
   });
 
   it('should respond with status 401 if there is no session for given token', async () => {
     const userWithoutSession = await createUser();
-    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+    const token = generateValidToken(userWithoutSession);
     const response = await server.get('/booking').set('Authorization', `Bearer ${token}`);
     expect(response.status).toBe(httpStatus.UNAUTHORIZED);
   });
@@ -79,7 +79,7 @@ describe('POST /booking', () => {
   });
 
   it('should respond with status 401 if given token is not valid', async () => {
-    const token = faker.lorem.word();
+    const token = jwt.sign({ userId: 1 }, process.env.JWT_SECRET);
     const response = await server.post('/booking').set('Authorization', `Bearer ${token}`);
     expect(response.status).toBe(httpStatus.UNAUTHORIZED);
   });
